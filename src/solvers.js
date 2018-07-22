ae07b77bccc6b9b0819b09330a6e3bf7890ec579
Philipp Albrecht <philippbalbrecht@gmail.com>
2018-07-22 15:09:49 -0700

    completed findNRooksSolution()

--- src/solvers.js
+++ src/solvers.js
@@ -10,17 +10,40 @@
 // (There are also optimizations that will allow you to skip a lot of the dead search space)
 // take a look at solversSpec.js to see what the tests are expecting
 
-
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
-
-
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  // Create empty matrix (makeEmptyMatrix(n))
+  var rookBoard = new Board({ n : n })
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
-};
+  // iterate through each row in empty matrix (forEach: row, index)
+  // rookBoard.rows().forEach(function(row, rowIndex) {
+  // //iterate through each col (forEach: col, index
+  //   row.forEach(function(col, colIndex){
+  // //if not (hasRowConflictAt && hasColConflictAt)
+
+  //     if (rookBoard.hasRowConflictAt(Indexcol) || rookBoard.hasRowConflictAt(Indexrow) ){
+  //     //toggle (row, col)
+  //        rookBoard.togglePiece(rowIndex, colIndex);
+  //     }
+
+  //   });
+  // });
+  for (var row = 0; row < rookBoard.rows().length; row++) {
+    for (var col = 0; col < rookBoard.rows()[row].length; col++) {
+      rookBoard.togglePiece(row, col);
+      if (rookBoard.hasRowConflictAt(row) || rookBoard.hasColConflictAt(col)) {
+      rookBoard.togglePiece(row, col);
+      }
+    }
+  }
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(rookBoard.rows()));
+ //return matrix
+  return rookBoard.rows();
+  }
+
+
+
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
